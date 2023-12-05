305711007285281ed8f61b3890f3bdfadd1aaec8
PriyanshuGB <priyanshusinsinwar@gmail.com>
2023-12-05 21:11:04 +0530

    Title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to my Reservation date app! 
+          Flight Schedule 
         </p>
         <a
           className="App-link"
